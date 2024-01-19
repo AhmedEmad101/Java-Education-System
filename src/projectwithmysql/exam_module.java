83ca374e66c427b5985d635ee098d468bfff8352
AhmedEmad101 <95496311+AhmedEmad101@users.noreply.github.com>
2024-01-19 23:02:41 +0200

    First commit

---
+++ src/projectwithmysql/exam_module.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectwithmysql;
+
+/**
+ *
+ * @author minam
+ */
+public class exam_module {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_loginexam frm=new frm_loginexam();
+        frm.setVisible(true);
+        frm.setLocationRelativeTo(null);
+        frm.setDefaultCloseOperation(2);
+    }
+    
+}
